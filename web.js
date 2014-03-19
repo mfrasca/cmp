07fd749508af2b57714a5b1a0ebda4413f42ff8d
Mario Frasca <mrgsfrasca@gmail.com>
2014-03-19 09:11:26 -0500

    adding minimal files.

---
+++ web.js
@@ -0,0 +1,28 @@
+// this is the remote server
+
+var express = require("express");
+var app = express();
+var port = Number(process.env.PORT || 5000);
+
+var fs = require('fs');
+
+String.prototype.formatU = function() {
+    var str = this.toString();
+    if (!arguments.length)
+        return str;
+    var args = typeof arguments[0];
+    args = (("string" == args || "number" == args) ? arguments : arguments[0]);
+    for (var arg in args)
+        str = str.replace(RegExp("\\{" + arg + "\\}", "gi"), args[arg]);
+    return str;
+};
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+    res.render("accueil");
+});
+
