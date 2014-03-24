2153f84dac2c3fd4ae541d529fc72b16b58a4dd7
Mario Frasca <mrgsfrasca@gmail.com>
2014-03-24 18:03:53 -0500

    work in progress, making it parametric, but data is still hard coded.

--- public/js/supercomputi.js
+++ public/js/supercomputi.js
@@ -1,19 +1,18 @@
-// this javascript element is part of bauble.classic
-// http://github.com/mfrasca/bauble.classic
+// this javascript element is part of cmp - contabilita lavori
+// http://github.com/mfrasca/cmp
 
-// bauble.classic is free software: you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation, either version 2 of the
-// License, or (at your option) any later version.
+// cmp is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 2 of the License, or (at your option) any
+// later version.
 
-// bauble.classic is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
+// cmp is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
 
-// You should have received a copy of the GNU General Public License
-// along with bauble.classic.  If not, see
-// <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along
+// with cmp.  If not, see <http://www.gnu.org/licenses/>.
 
 
 //
@@ -52,6 +51,160 @@ function choose(obj) {
     $(linea).children($(obj).attr("lettera")).removeClass("hidden");
 }
 
+function load_computo() {
+    var computo = [
+        { tipo: 'codice',
+          codice: '01 001',
+        },
+        { tipo: 'descrizione',
+          testo: 'descrizione dell&apos;articolo riferito',
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'fattori',
+          qq: 1,
+          ln: 1.0,
+          lr: 0.0,
+          hh: 0.0,
+        },
+        { tipo: 'relativo',
+          qq: 1,
+          ln: 1.0,
+        },
+    ];
+
+    $.each(computo, function(index, linea) {
+        var id_linea = "l" + (10000 + index).toFixed(0).substr(1) + "-00";
+        var lineadicomputo = $("<div/>", { id: id_linea,
+                                           class: "lineadicomputo",
+                                         });
+        var tipolinea = $("<div/>", { class: "tipolinea" });
+        lineadicomputo.append(tipolinea);
+        $.each([{l: 'codice', t: 'Ⓒ'},
+                {l: 'descrizione', t: 'Ⓓ'},
+                {l: 'fattori', t: 'Ⓕ'},
+                {l: 'relativo', t: 'Ⓡ'},
+               ], function(ignore, tipo) {
+                   var elm = $("<div/>", { class: "choose",
+                                           lettera: "." + tipo.l,
+                                           onclick: "choose(this);",
+                                         });
+                   if (linea.tipo === tipo.l) 
+                       elm.addClass("chosen");
+                   elm.text(tipo.t);
+                   tipolinea.append(elm);
+               });
+        $.each([{ part: 'codice', content: ['cod'] },
+                { part: 'descrizione', content: ['desc']},
+                { part: 'fattori', content: ['qq', 'ln', 'lr', 'hh'] },
+                { part: 'relativo', content: ['qq', 'ln'] },
+               ], function(i, elm) {
+                   var part = $("<div/>", { id: id_linea + "-" + elm.part,
+                                            class: "cdfr hidden " + elm.part });
+                   lineadicomputo.append(part);
+                   $.each(elm.content, function(j, what) {
+                       var input = $("<input/>", {class: what});
+                       switch(elm.part){
+                       case 'fattori':
+                       case 'relativo':
+                           input.addClass('fattore');
+                           break;
+                       case 'descrizione':
+                           input.addClass('ds');
+                           break;
+                       }
+                       part.append(input);
+                   });
+               });
+        $("#computo").append(lineadicomputo);
+        var obj = $('#' + id_linea + '-' + linea.tipo);
+        obj.removeClass('hidden');
+        obj.addClass('shown');
+        switch(linea.tipo) {
+        case 'codice':
+            obj.children('input').attr({value: linea.codice});
+            break;
+        case 'descrizione':
+            obj.children('input').attr({value: linea.descrizione});
+            break;
+        case 'fattori':
+            obj.children('input#qq').attr({value: linea.qq});
+            obj.children('input#ln').attr({value: linea.ln});
+            obj.children('input#lr').attr({value: linea.lr});
+            obj.children('input#hh').attr({value: linea.hh});
+            break;
+        case 'relativo':
+            obj.children('input#qq').attr({value: linea.qq});
+            obj.children('input#ln').attr({value: linea.ln});
+            break;
+        }
+    });
+}
+
 // to be called at document ready!
 function init() {
 
@@ -88,5 +241,5 @@ function init() {
 
 
     });
-
+    load_computo();
 }
